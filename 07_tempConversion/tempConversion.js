13d8063db1ebd84d6a4e74ecac5230f14991b3f8
Shubham Jha <167497003+sj-shub@users.noreply.github.com>
2024-11-30 00:23:28 +0530

    Add code to convert string returned by toFixed to number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let tempInC=((tempInF-32)*(5/9));
+  return parseFloat(tempInC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+ let tempInF=((tempInC*(9/5))+32);
+  return parseFloat(tempInF.toFixed(1));
 };
 
 // Do not edit below this line
