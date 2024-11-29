f33ad8348d8c6b6194e336e3b5362a5a50c89429
Shubham Jha <167497003+sj-shub@users.noreply.github.com>
2024-11-29 23:58:37 +0530

    Add code for leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(year%4===0){
+        if(year%100===0){
+            if(!(year%400===0)) return false;
+            else return true;
+        }
+        return true;
+    }
+    else return false;
 
 };
 
